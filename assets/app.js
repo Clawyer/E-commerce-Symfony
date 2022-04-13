cc0552903bc0114ebbb8455e4843a5ff44f4232f
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-13 11:15:06 +0200

    fix

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import "./website/styles/reset.css";
+// import "./website/styles/reset.css";
 import "./website/styles/footer.css";
 require("./website/styles/header.sass");
 
